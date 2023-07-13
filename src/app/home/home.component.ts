e6158a2a764ab873fed47827d8854bf72b5d55a4
srikanthyaswada <yaswadasrikanth@gmail.com>
2023-07-13 17:22:50 +0530

    Completed mygallerybook

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
- 
+  statusClass = 'not-active';
+
+  scrollToContact(element: any) {
+    element.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+      inline: 'nearest',
+    });
+  }
+  scrollToShowcase(element: any) {
+    element.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+      inline: 'nearest',
+    });
+  }
+  scrollToPrice(element: any) {
+    element.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+      inline: 'nearest',
+    });
+  }
+  scrollToFeatures(element: any) {
+    element.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+      inline: 'nearest',
+    });
+  }
+  scrollToAbout(element: any) {
+    element.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+      inline: 'nearest',
+    });
+  }
+  scrollToHome(element: any) {
+    element.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+      inline: 'nearest',
+    });
+    this.statusClass = 'active';
+  }
 }
